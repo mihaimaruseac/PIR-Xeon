fadace3c9b8568e11ab3b4365662ad385507e308
Mihai Maruseac <mihai.maruseac@gmail.com>
2016-04-14 20:44:19 -0400

    Also test number of OpenMP threads

--- ko.c
+++ ko.c
@@ -5,6 +5,7 @@
 #include <gmp.h>
 
 #include <cilk/cilk.h>
+#include <omp.h>
 
 #include "client.h"
 #include "globals.h"
@@ -117,6 +118,7 @@ int main(int argc, char **argv)
 			state, prime, &minvp, numbers);
 	printf("%d %lu %d\n", mp_bits_per_limb, mpz_size(prime), args.keysize / mp_bits_per_limb);
 	printf("Cilk %d workers\n", __cilkrts_get_nworkers());
+	printf("OpenMP %d threads\n", omp_get_num_threads());
 
 	server(args.db_size, prime, minvp, args.query_length,
 			(const mpz_t *)numbers, num_outputs, results);
