2fa12547a83c796954a0b3a89735afa5ba15ef01
Mihai Maruseac <mihai.maruseac@gmail.com>
2016-04-16 18:47:57 -0400

    Better parse of experiments

--- csver.py
+++ csver.py
@@ -22,11 +22,11 @@ class Exp:
 
 exps = {}
 for fname in sys.argv[1:]:
-    exp = Exp()
     with open(fname, "r") as f:
         line = f.readline()                          # Called with: argc=7
         while line:
             assert line.startswith("Called")
+            exp = Exp()
             line = f.readline().split()              # ./ko -m 1024 -n 65536 -k 8
             for i in [1,3,5]:
                 exp.record_key(line[i][1], line[i+1])
@@ -34,18 +34,20 @@ for fname in sys.argv[1:]:
                 line = f.readline()                  # ...
                 if line.startswith("Total time:"):   # Total time:  75.109 ms
                     exp.record_value_from_line("tt", line)
-                elif line.startswith("Time/multp:"): # Time/multp:   0.001 ms
-                    exp.record_value_from_line("tpm", line)
-                elif line.startswith("Time/round:"): # Time/round:   0.009 ms
-                    exp.record_value_from_line("tpr", line)
-                elif line.startswith("Ops/second:"): # Ops/second:   0.873 mmps
-                    exp.record_value_from_line("mmps", line)
-                elif not line:
-                    break
-                else:
                     continue
-
-    exps[tuple(exp.key.items())] = exp.value.items()
+                if line.startswith("Time/multp:"):   # Time/multp:   0.001 ms
+                    exp.record_value_from_line("tpm", line)
+                    continue
+                if line.startswith("Time/round:"):   # Time/round:   0.009 ms
+                    exp.record_value_from_line("tpr", line)
+                    continue
+                if line.startswith("Ops/second:"):   # Ops/second:   0.873 mmps
+                    exp.record_value_from_line("mmps", line)
+                    exps[tuple(exp.key.items())] = exp.value.items()
+                    line = f.readline()              # Called with: argc=7
+                    break
+                if not line:
+                    break
 
 columns=["m","n","k","tt","tpm","tpr","mmps"]
 print ','.join(columns)
